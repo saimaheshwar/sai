182ab4e63b9a66e70c700bbe7823631c9bd1c61f
saimaheshwar <60170775+saimaheshwar@users.noreply.github.com>
2020-01-30 20:10:41 +0530

    Add files via upload

---
+++ polymorphism1.java
@@ -0,0 +1,16 @@
+class Addition{  
+    static void sum(int a,int b){
+        System.out.println("Area is:"+ (a+b));
+    }
+    
+    static void sum(int a,int b,int c){
+        System.out.println("Area is:"+ (a+b+c)); 
+}  
+}
+public class Demo{  
+    public static void main(String[] args){  
+    Addition.sum(11,11);  
+     
+    }
+    
+}
